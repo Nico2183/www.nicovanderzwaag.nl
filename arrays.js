ecac81c76b557d64ff5378595e63db8cd6f94137
Nico Van der Zwaag <79093074+Nico2183@users.noreply.github.com>
2021-03-23 08:30:43 +0100

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,103 @@
+var werkervaring = [{
+    beginjaar: '2019',
+    eindjaar: '2019',
+    werkgever: 'AdFranse Automatisering B.V',
+    functie: 'Medewerker technische dienst',
+    functieomschrijving: 'Het installeren van software en hardware. Reparaties van laptops, desktops. Documenteren van de uitgevoerde reparaties, installaties. klanten bellen. helpdesk werkzaamheden',
+  },
+  {
+    beginjaar: '2011',
+    eindjaar: '2019',
+    werkgever: 'Van de Swaluw B.V. is sinds 1-01-2018 G. Bosman Transport',
+    functie: 'Chauffeur internationaal',
+    functieomschrijving: 'Het vervoer van voornamelijk los gestorte goederen met een walking floor, tevens ook pallet goederen van A naar B (Nederland-België-Frankrijk-Engeland-Italië-Duitsland)',
+  }
+];
+
+var opleidingen = [{
+    beginjaar: '2021',
+    eindjaar: '2021',
+    schoolnaam: 'Bit-acedemy',
+    studie: 'Full stack development',
+    diploma: 'Verwacht medio april 2021',
+  },
+  {
+    beginjaar: '2020',
+    eindjaar: '2021',
+    schoolnaam: 'Slim In ICT!',
+    studie: 'Front-end development',
+    diploma: 'Ja',
+  },
+  {
+    beginjaar: '2000',
+    eindjaar: '2002',
+    schoolnaam: 'Markizaat college',
+    studie: 'Chauffeur goederenvervoer',
+    diploma: 'Ja',
+  },
+  {
+    beginjaar: '1996',
+    eindjaar: '2000',
+    schoolnaam: 'Csw Middelburg',
+    studie: 'Vbo verzorging',
+    diploma: 'Ja',
+  }
+];
+
+var computerskills = [ //Voeg voor elke skill een level in tussen 0 en 5, vijf is het hoogste.
+
+  {
+    skill: 'Html',
+    logo: 'images/logos/html.png',
+    level: 5,
+    stars: '★★★★★'
+  },
+  {
+    skill: 'Css',
+    logo: 'images/logos/css.png',
+    level: 5,
+    stars: '★★★★★'
+  },
+  {
+    skill: 'JavaScript',
+    logo: 'images/logos/js.png',
+    level: 5,
+    stars: '★★★★★'
+  },
+  {
+    skill: 'Bootstrap',
+    logo: 'images/logos/bootstrap.png',
+    level: 5,
+    stars: '★★★★★'
+  },
+  {
+    skill: 'AngularJs',
+    logo: 'images/logos/angular.png',
+    level: 3,
+    stars: '★★★☆☆'
+  },
+  {
+    skill: 'jQuery',
+    logo: 'images/logos/jq.png',
+    level: 3,
+    stars: '★★★☆☆'
+  },
+  {
+    skill: 'Php',
+    logo: 'images/logos/php.png',
+    level: 2,
+    stars: '★★☆☆☆'
+  },
+  {
+    skill: 'Apple devices',
+    logo: 'images/logos/apple.png',
+    level: 5,
+    stars: '★★★★★'
+  },
+  {
+    skill: 'Windows devices',
+    logo: 'images/logos/win.png',
+    level: 5,
+    stars: '★★★★★'
+  }
+]
